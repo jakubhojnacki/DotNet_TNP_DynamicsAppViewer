9c6c77714d87a2cb104bb4297318994c80a55b5d
Jakub Hojnacki <jakubhojnacki@gmail.com>
2023-03-25 19:29:44 +0000

    Logo, about dialog

--- Source/AboutView.cpp
+++ Source/AboutView.cpp
@@ -31,4 +31,10 @@ void AboutView::initialiseController(const AboutController* pController) {
 
 void AboutView::initialiseWindow() {
     this->setWindowTitle(this->controller->title());
+
+    this->ui->nameLabel->setText(this->controller->name());
+    this->ui->descriptionLabel->setText(this->controller->description());
+    this->ui->versionLabel->setText(this->controller->version().toString());
+    this->ui->authorLabel->setText(this->controller->author());
+    this->ui->dateLabel->setText(this->controller->date());
 }
